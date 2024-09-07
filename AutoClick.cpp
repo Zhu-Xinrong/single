af97519663fc11b6cecb7a17cd25bdc738d7a65e
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-07 09:23:28 +0800

    refactor: 重构至面向对象

--- AutoClick.cpp
+++ AutoClick.cpp
@@ -1,11 +1,51 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<windows.h>
-int main(){
+class CAutoClick{
+    private:
+        char button;
+        int click,counter;
+        DWORD sleepTime;
+    protected:
+        auto clickBase(){
+            switch(button){
+                case 'L':{
+                    mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+                    break;
+                }case 'M':{
+                    mouse_event(MOUSEEVENTF_MIDDLEDOWN|MOUSEEVENTF_MIDDLEUP,0,0,0,0);
+                    break;
+                }case 'R':{
+                    mouse_event(MOUSEEVENTF_RIGHTDOWN|MOUSEEVENTF_RIGHTUP,0,0,0,0);
+                    break;
+                }
+            }
+        }
+    public:
+        CAutoClick():
+            button{},click{},counter{1},sleepTime{}{}
+        ~CAutoClick(){}
+        auto set(char button,int click,DWORD sleepTime){
+            this->button=button;
+            this->click=click;
+            this->sleepTime=sleepTime;
+        }
+        auto run(){
+            for(;counter<=click;++counter){
+                clickBase();
+                if(counter==click){
+                    break;
+                }
+                Sleep(sleepTime);
+            }
+        }
+};
+auto main()->int{
     while(true){
+        CAutoClick c;
         int click{};
 		DWORD sleepTime{};
-		char key[2]{};
+		char button[2]{};
         printf("输入点击次数: ");
         while(true){
             scanf("%d",&click);
@@ -22,10 +62,10 @@ int main(){
             }
 			printf("输入数据必须大于 0, 请重新输入: ");
 		}
-        printf("按下左键 (0), 右键 (1) 还是中键 (2): ");
+        printf("按下左键 (L), 中键 (M), 还是右键 (R): ");
 		while(true){
-            scanf("%s",key);
-            if(((key[0]=='0')||(key[0]=='1')||(key[0]=='2'))&&(key[1]==0)){
+            scanf("%s",button);
+            if(((button[0]=='L')||(button[0]=='M')||(button[0]=='R'))&&(button[1]==0)){
 				break;
             }
             printf("输入错误, 请重新输入: ");
@@ -37,22 +77,11 @@ int main(){
             }
             Sleep(1000);
         }
-        for(int i{};i<click;++i){
-            switch(key[0]){
-                case '0':{
-                    mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
-                    break;
-                }case '1':{
-                    mouse_event(MOUSEEVENTF_RIGHTDOWN|MOUSEEVENTF_RIGHTUP,0,0,0,0);
-                    break;
-                }case '2':{
-                    mouse_event(MOUSEEVENTF_MIDDLEDOWN|MOUSEEVENTF_MIDDLEUP,0,0,0,0);
-                    break;
-                }
-            }
-            Sleep(sleepTime);
-        }
-        printf("\n");
+        puts("\n正在应用配置.");
+        c.set(button[0],click,sleepTime);
+        puts("开始执行.");
+        c.run();
+        puts("执行完毕.\n");
     }
     return 0;
 }
