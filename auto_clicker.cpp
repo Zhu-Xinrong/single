9357c1f0b28b22f41c86dfc13f5bed55bc7fbed4
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-04-04 20:04:18 +0800

    refactor

--- auto_clicker.cpp
+++ auto_clicker.cpp
@@ -110,15 +110,15 @@ auto main() -> int
     const auto current_window_handle{ GetConsoleWindow() };
     const auto std_input_handle{ GetStdHandle( STD_INPUT_HANDLE ) };
     const auto std_output_handle{ GetStdHandle( STD_OUTPUT_HANDLE ) };
-    cpp_utils::set_console_charset( 54936 );
-    cpp_utils::set_console_title( "Auto Clicker" );
-    cpp_utils::set_console_size( current_window_handle, std_output_handle, 50, 25 );
-    cpp_utils::fix_window_size( current_window_handle, true );
     cpp_utils::ignore_console_exit_signal( true );
     cpp_utils::enable_window_minimize_ctrl( current_window_handle, false );
     cpp_utils::enable_window_maximize_ctrl( current_window_handle, false );
     cpp_utils::enable_window_close_ctrl( current_window_handle, false );
     cpp_utils::enable_window_menu( current_window_handle, false );
+    cpp_utils::set_console_charset( 54936 );
+    cpp_utils::set_console_title( "Auto Clicker" );
+    cpp_utils::set_console_size( current_window_handle, std_output_handle, 50, 25 );
+    cpp_utils::fix_window_size( current_window_handle, true );
     cpp_utils::console_ui ui{ std_input_handle, std_output_handle };
     ui.add_back( "                    Auto Clicker\n\n" )
       .add_back( " (i) 全部设置后即可执行.\n" )
